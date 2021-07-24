138e9cb8bf04a59ba8eb054e8aaf0fa75cd9d635
Alan Toa <37520667+MonsterAnan@users.noreply.github.com>
2021-07-24 22:24:03 +0800

    fix:ERROR: `currentlyFocusedField is deprecated `.
    currentlyFocusedField is deprecated and will be removed in a future release. 
    currentlyFocusedField replace currentlyFocusedInput!

--- src/KeyboardAvoidingContainer.tsx
+++ src/KeyboardAvoidingContainer.tsx
@@ -218,7 +218,7 @@ export function useKeyboardAvoidingContainerProps<
         if (keyboardLayoutRef.current) return
 
         const {endCoordinates: newKeyboardLayout} = event
-        const newFocusedTextInputNodeHandle = NativeTextInput.State.currentlyFocusedField()
+        const newFocusedTextInputNodeHandle = NativeTextInput.State.currentlyFocusedInput()
         const newStickyFooterNodeHandle = findNodeHandle(
           stickyFooterRef.current,
         )
